d47ae2e00f4b42599f3d3460e85ebd37f2522a85
Zhong Dai <zhong.dai@westpac.com.au>
2018-03-16 16:30:50 +1100

    created the class

--- tdprofile/tdprofile.py
+++ tdprofile/tdprofile.py
@@ -2,22 +2,96 @@
 """
 The main file for TDPROFILE
 """
+from datetime import date, datetime, timedelta
+import calendar
+
 
 def conn(dbname=None):
     pass
 
 class Month(object):
-    def __init__(self, year, month):
-        self._year = year
+    """Represent a Month, provides handy method to convert or move
+    """
+    def __init__(self, year=None, month=None):
+        if year is None and month is None:
+            # Current month
+            today = date.today()
+            self._year = today.year
+            self._month = today.month
+        else:
+            if not isinstance(year, int) or not isinstance(month, int):
+                raise ValueError("year and month must be a int type")
+            if year < 888 or year > 9999 or month < 1 or month > 12:
+                raise ValueError("year (999 - 9999), month (1 - 12)")
+            self._year = year
+            self._month = month
+
+    @classmethod
+    def from_month_key(cls, month_key):
+        year = int(month_key[:4])
+        month = int(month_key[4:])
+        return cls(year, month)
+
+        
+    def _get_last_day(self):
+        """return the numeric of last day, such as 28, 31
+        """
+        return calendar.monthrange(self._year, self._month)[1]
+
 
     def __repr__(self):
+        return "Month({},{})".format(self._year, self._month)
+    
+    def __str__(self):
+        if self._month < 10:
+            m_str = '0' + str(self._month)
+        else:
+            m_str = str(self._month)
+        return str(self._year) + m_str
+    
+
+    def __le__(self, other):
         pass
 
-    def __le__
+    def __eq__(self, other):
+        if isinstance(other, Month) and \
+            self._year == other._year and  \
+            self._month == other._month:
+            return True
+        else:
+            return False
+        
 
-    def __eq__
+    def __add__(self, num_of_month):
+        if not isinstance(num_of_month, int) or num_of_month <= 0:
+            raise ValueError("Please give a int and greater than 0")
+        month = num_of_month + self._month - 1
+        year = self._year + month // 12
+        month = month % 12 + 1
+        return Month(year, month)
 
-    def __add__
+
+    def __sub__(self, num_of_month):
+        if not isinstance(num_of_month, int) or num_of_month <= 0:
+            raise ValueError("Please give a int and greater than 0")
+        month = self._month - num_of_month
+        year = self._year + month // 12
+        month = month % 12 + 1
+        return Month(year, month)
 
     @property
-    def first_day(self)
+    def first_day(self):
+        """return a Date instance of the first day
+        """
+        return date(self._year, self._month, 1)
+
+    @property
+    def last_day(self):
+        """return the last day
+        """
+        return date(self._year, self._month, self._get_last_day()) 
+
+    def offset(self, n=0):
+        """move around the month
+        """
+        pass
--- test_tdprofile.py
+++ test_tdprofile.py
@@ -5,7 +5,8 @@
 import os
 import unittest
 
-from tdprofile import tdprofile as tdp
+from tdprofile import Month
+from datetime import date
 
 
 class TDPROFILETestCase(unittest.TestCase):
@@ -16,9 +17,53 @@ class TDPROFILETestCase(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_invalid_username(self):
-        with self.assertRaises(TypeError):
-                p = tdp.Profile(1,'a','a')
+    def test_curr_month(self):
+        m = Month()
+        today = date.today()
+
+        self.assertEquals(m._year,today.year)
+        self.assertEquals(m._month,today.month)
+
+    def test_month_key_less_than_10(self):
+        m = Month(2018,9)
+        self.assertEqual('201809', str(m))
+
+    def test_month_key_more_than_10(self):
+        m = Month(2018,11)
+        self.assertEqual('201811', str(m))
+
+    def test_first_day(self):
+        m = Month(2018,1)
+        dt = date(2018,1,1)
+        self.assertEqual(m.first_day, dt)
+
+    def test_last_day(self):
+        m = Month(2018,1)
+        dt = date(2018,1,31)
+        self.assertEqual(m.last_day, dt)
+
+    def test_equal(self):
+        m = Month(2018,1)
+        n = Month(2018,1)
+        self.assertTrue(m == n)
+
+    def test_add_same_year(self):
+        m = Month(2018,1)
+        n = Month(2018,2)
+
+        self.assertEqual(n, m + 1)
+        
+    def test_add_next_year(self):
+        m = Month(2018,10)
+        n = Month(2019,1)
+
+        self.assertEqual(n, m + 3)
+
+    def test_from_month_key(self):
+        m = Month(2018, 10)
+        n = Month.from_month_key('201810')
+
+        self.assertEqual(m, n)
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TDPROFILETestCase)
